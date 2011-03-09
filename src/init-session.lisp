77298b52cc9a07a3e97c3aa48853cd88c578b582
Nandan Bagchee <nandan@infinitelybeta.com>
2011-03-09 14:46:49 +0530

    - reduce LOC by removing unused parts

--- src/init-session.lisp
+++ src/init-session.lisp
@@ -7,18 +7,10 @@
 ;; 3 utility fns for cl-persistence
 
 (defun all-of (type)
-  "Accepts an argument symbol, finds stored objects of that type"
-  (declare (ignore arg))
   (find-persistent-objects (class-store type) type :order-by (cons 'name :asc)))
 
 (defun o-save (object)
   (persist-object *default-store* object))
-
-(defun o-delete (object)
-  "Delete an object from the store. Performs check first."
-  (when object
-    (awhen (object-id object)
-      (delete-persistent-object *default-store* object))))
 
 (defun render-question (q parent)
    (with-html (:table
@@ -31,27 +23,23 @@
                                       "dislike"))))))
 
 (defmethod render-widget-body ((obj firstpage) &rest args)
-    (with-html (:p "List of top-voted questions for Weblocks tutorial")
+    (with-html (:p "What shall we cover?"
+                   (render-link (f_% (let ((newq (make-instance 'question :title "")))
+                                       (do-dialog " + question" (make-instance 'dataform :data newq
+                                                                               :ui-state :form
+                                                                               :on-success (f_ (mark-dirty (root-composite)) (answer _))
+                                                                               :on-cancel (f_ (answer _))
+                                                                               :form-view 'question-form-view))))
+                                " + question"))
                (let ((q (all-of 'question)))
                  (dolist (q (stable-sort q #'> :key #'upvotes-of))
                    (render-widget (make-widget (f_% (render-question q obj))))))))
                                          
 
 (defview question-form-view (:type form)
-  (id :hidep t)
-  (upvotes :parse-as integer :hidep t)
-  (downvotes :parse-as integer :hidep t)
   (title))
   
-
 (defun init-user-session (root)
   (setf (widget-children root)
 	(list
-         (make-widget (f_% (render-link (f_% (let ((newq (make-instance 'question :title "")))
-                                               (do-dialog "Add question" (make-instance 'dataform :data newq
-                                                                                        :ui-state :form
-                                                                                        :on-success (f_ (mark-dirty (root-composite)) (answer _))
-                                                                                        :on-cancel (f_ (answer _))
-                                                                                        :form-view 'question-form-view))))
-                           "Add new question")))
          (make-instance 'firstpage))))
